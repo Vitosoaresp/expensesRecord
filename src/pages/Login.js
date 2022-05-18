9fba5cf4e6e611dc0c0042b9451ab2b6e7c042df
Vitor Soares <pereiravitor1218@gmail.com>
2022-05-18 16:10:34 -0300

    adc referencia para validaçao de email

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,6 +20,7 @@ class Login extends React.Component {
   };
 
   IsEmail = (email) => {
+  // https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
     const re = /\S+@\S+\.\S+/;
     return !re.test(email);
   };
